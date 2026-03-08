8b3aaff5e1591913ba748b171b9e51a4e9047dd6
cerealarsonist <mcsjindani@tip.edu.ph>
2026-03-08 17:26:21 +0800

    New URL Favicon

--- store/urls.py
+++ store/urls.py
@@ -5,3 +5,9 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('add/', views.add_product, name='add_product'),
 ]
+
+from django.http import HttpResponse
+
+urlpatterns += [
+    path('favicon.ico', lambda request: HttpResponse(status=204)),  # 204 = No Content
+]
